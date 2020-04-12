320ba65a173fce58fb98a63b942b0100816c9ac7
ah12068 <dang.jeremy01@gmail.com>
2020-04-12 18:18:21 +0100

    edited visuals script

--- src/models/constants.py
+++ src/models/constants.py
@@ -1,5 +1,4 @@
 from sklearn.linear_model import LogisticRegression
-from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -8,11 +7,9 @@ random_seed = 1
 best_model_file_name = 'final_model.pkl'
 
 baseline_classifiers = {
-    "LogisiticRegression": LogisticRegression(random_state=random_seed, max_iter=200),
+    "LogisticRegression": LogisticRegression(random_state=random_seed, max_iter=200),
     "KNearest": KNeighborsClassifier(),
-    "Support Vector Classifier": SVC(random_state=random_seed),
-    "DecisionTreeClassifier": DecisionTreeClassifier(random_state=random_seed),
-    "RandomForestClassifier": RandomForestClassifier(random_state=random_seed)
+    "DecisionTreeClassifier": DecisionTreeClassifier(random_state=random_seed)
 }
 
 LogisiticRegression_grid = {
